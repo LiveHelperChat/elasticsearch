5eb35d9974a08750df8915f010b6326083bd52f8
Remdex <remdex@gmail.com>
2017-07-25 09:13:45 +0200

    Change default settings

--- settings/settings.ini.default.php
+++ settings/settings.ini.default.php
@@ -1,11 +1,18 @@
 <?php 
 
-return array(
-    'host' => '<host_process>',
-    'ahosting' => true,
-    'is_enabled' => true,
-    'buttonid' => '<button_id>',   
-    'customfields' => '<customfield|Value>'
+return array (
+		'host' 			=> 'localhost',//'localhost',		
+		'port' 			=> '9200',
+		'index' 		=> 'chat',	
+	    'additional_indexes' => array(
+	        
+	    ),
+	    'use_iam' => false,
+	    'iam_region' => 'eu-central-1',
+	    'iam_credentials' => array(
+	        'access_key' => '',
+	        'secret_key' => ''
+	    )
 );
 
 ?>
