010c18b876e3ca2e022c2714049fec930d027983
Remdex <remdex@gmail.com>
2017-09-11 16:11:12 +0200

    check is set

--- design/elasticsearchtheme/tpl/lhchat/chat_tabs/extension_chat_tab_content_multiinclude.tpl.php
+++ design/elasticsearchtheme/tpl/lhchat/chat_tabs/extension_chat_tab_content_multiinclude.tpl.php
@@ -1,4 +1,4 @@
-<?php if ($isEnabledElasticUsername == true) : ?>
+<?php if (isset($isEnabledElasticUsername) && $isEnabledElasticUsername == true) : ?>
 <div role="tabpanel" class="tab-pane" id="online-user-info-eschats-tab-<?php echo $chat->id?>">
 
 </div>
