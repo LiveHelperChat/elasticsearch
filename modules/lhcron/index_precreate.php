ed2f1c4486a5c62394eb025ef8bca11006180b5e
Remdex <remdex@gmail.com>
2018-03-02 14:37:30 +0100

    Precreate index by cronjob

---
+++ modules/lhcron/index_precreate.php
@@ -0,0 +1,28 @@
+<?php
+
+// Run once a day
+// /usr/bin/php cron.php -s site_admin -e elasticsearch -c cron/index_precreate
+
+echo "Indexing online sessions\n";
+
+$esOptions = erLhcoreClassModelChatConfig::fetch('elasticsearch_options');
+$dataOptions = (array)$esOptions->data;
+
+$settings = include ('extension/elasticsearch/settings/settings.ini.php');
+
+$settings['index'];
+
+$indexSave = null;
+
+if ($dataOptions['index_type'] == 'daily') {
+    $indexSave = $settings['index'] . date('Y.m.d',time()+24*3600);
+} elseif ($dataOptions['index_type'] == 'monthly') {
+    $indexSave = $settings['index'] . date('Y.m',time()+24*3600);
+}
+
+if ($indexSave !== null) {
+    $sessionElasticStatistic = erLhcoreClassModelESChat::getSession();
+    $esSearchHandler = erLhcoreClassElasticClient::getHandler();
+    erLhcoreClassElasticClient::indexExists($esSearchHandler, $indexSave);
+    echo "Created index - ",$indexSave,"\n";
+}
