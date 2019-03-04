e7cfbbf902302f89257232aa8934e53698b4f66e
Remigijus Kiminas <remdex@gmail.com>
2019-03-04 09:09:30 -0500

    do not show link if not required

--- design/elasticsearchtheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
+++ design/elasticsearchtheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
@@ -1,1 +1,3 @@
+<?php if (erLhcoreClassUser::instance()->hasAccessTo('lhelasticsearch','use_admin')) : ?>
 <li class="nav-item"><a class="nav-link" href="<?php echo erLhcoreClassDesign::baseurl('elasticsearch/index')?>"><i class="material-icons">&#xE8B6;</i>ElasticSearch</a></li>
+<?php endif; ?>
