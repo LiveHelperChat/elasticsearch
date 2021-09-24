81648b2695646902d3e252c96d8819c15eaf5fd7
Remigijus Kiminas <remdex@gmail.com>
2021-09-24 09:55:32 -0400

    Update structure

--- doc/install.sql
+++ doc/install.sql
@@ -3,3 +3,11 @@ CREATE TABLE `lhc_lheschat_index` (
   `status` tinyint(1) unsigned NOT NULL DEFAULT 0,
   UNIQUE KEY `chat_id` (`chat_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `lhc_lhesmail_index` (
+                                      `mail_id` bigint(20) unsigned NOT NULL,
+                                      `status` tinyint(1) unsigned NOT NULL DEFAULT 0,
+                                      `op` tinyint(1) unsigned NOT NULL DEFAULT 0,
+                                      `udate` bigint(20) unsigned NOT NULL DEFAULT 0,
+                                      UNIQUE KEY `mail_id_op` (`mail_id`,`op`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
