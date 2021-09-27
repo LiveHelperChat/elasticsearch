e2c27a505b947ee481625ae08bf18de6b87140bb
Remigijus Kiminas <remdex@gmail.com>
2021-09-27 00:17:14 -0400

    Faster way to delete indices

--- design/elasticsearchtheme/tpl/elasticsearch/indices.tpl.php
+++ design/elasticsearchtheme/tpl/elasticsearch/indices.tpl.php
@@ -1,18 +1,27 @@
 <h1>Indices</h1>
 
-<table class="table">
-    <thead>
+<form action="" method="post">
+    <table class="table">
+        <thead>
+            <tr>
+                <th width="1%"></th>
+                <th>Indice</th>
+                <th width="1%"></th>
+            </tr>
+        </thead>
+        <?php foreach ($indices as $indice => $alias) : ?>
         <tr>
-            <th>Indice</th>
-            <th width="1%"></th>
+            <td><input type="checkbox" value="<?php echo htmlspecialchars($indice)?>" name="indices[]" ></td>
+            <td><a href="<?php echo erLhcoreClassDesign::baseurl('elasticsearch/getmapping')?>/<?php echo htmlspecialchars($indice)?>"><?php echo htmlspecialchars($indice) ?></a></td>
+            <td><a class="btn btn-danger btn-xs csfr-required" onclick="return confirm('<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('kernel/messages','Are you sure?');?>')" href="<?php echo erLhcoreClassDesign::baseurl('elasticsearch/deleteindice')?>/<?php echo htmlspecialchars($indice)?>">Delete</a></td>
         </tr>
-    </thead>
-    <?php foreach ($indices as $indice => $alias) : ?>
-    <tr>
-        <td><a href="<?php echo erLhcoreClassDesign::baseurl('elasticsearch/getmapping')?>/<?php echo htmlspecialchars($indice)?>"><?php echo htmlspecialchars($indice) ?></a></td>
-        <td><a class="btn btn-danger btn-xs csfr-required" onclick="return confirm('<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('kernel/messages','Are you sure?');?>')" href="<?php echo erLhcoreClassDesign::baseurl('elasticsearch/deleteindice')?>/<?php echo htmlspecialchars($indice)?>">Delete</a></td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+        <?php endforeach; ?>
+    </table>
 
+    <?php include(erLhcoreClassDesign::designtpl('lhkernel/csfr_token.tpl.php'));?>
+
+    <div class="form-group">
+        <button type="submit" name="DeleteIndices" class="btn btn-danger" value="delete">Delete selected</button>
+    </div>
+</form>
 <?php include(erLhcoreClassDesign::designtpl('lhkernel/secure_links.tpl.php')); ?>
--- modules/lhelasticsearch/indices.php
+++ modules/lhelasticsearch/indices.php
@@ -1,6 +1,22 @@
 <?php
 $tpl = erLhcoreClassTemplate::getInstance('elasticsearch/indices.tpl.php');
 
+
+if (ezcInputForm::hasPostData()) {
+
+    if (!isset($_POST['csfr_token']) || !$currentUser->validateCSFRToken($_POST['csfr_token'])) {
+        erLhcoreClassModule::redirect('elasticsearch/indices');
+        exit;
+    }
+
+    foreach ($_POST['indices'] as $indice) {
+        erLhcoreClassElasticClient::getHandler()->indices()->delete(array(
+            'index' => $indice
+        ));
+    }
+}
+
+
 $tpl->set('indices',erLhcoreClassElasticClient::getHandler()->indices()->getAliases(array('index' => 'chat*')));
 
 $Result['content'] = $tpl->fetch();
