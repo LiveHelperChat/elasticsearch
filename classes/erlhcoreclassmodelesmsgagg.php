5747722ed08846c81945272c8fc9feffcdc7cf72
allwarezz <dulissedaniele@yahoo.it>
2021-10-27 12:15:26 +0200

    Add class for simple aggragation

---
+++ classes/erlhcoreclassmodelesmsgagg.php
@@ -0,0 +1,37 @@
+<?php
+
+class erLhcoreClassModelESMsgAgg
+{
+    use erLhcoreClassElasticTrait;
+    
+    public function getState()
+    {
+        $states = array(
+            'key' => $this->key,
+            'doc_count' => $this->doc_count
+        );
+
+        return $states;
+    }
+
+    public function __get($var)
+    {
+        switch ($var) {
+
+            case 'demo':
+                $this->demo = '';
+                return $this->demo;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static $elasticType = 'lh_msg';
+
+    public $key = null;
+
+    public $doc_count = null;
+    
+    public $aggregationName = null;
+}
