b3539cdec22e6526c11b3c8d028c0b6e6cf68cc9
Remigijus Kiminas <remdex@gmail.com>
2021-12-21 00:46:28 -0500

    Separte template for subject filter

--- design/elasticsearchtheme/tpl/elasticsearch/parts/filter.tpl.php
+++ design/elasticsearchtheme/tpl/elasticsearch/parts/filter.tpl.php
@@ -91,20 +91,7 @@
                 </div>
             </div>
 		</div>
-        <div class="col-md-2">
-            <div class="form-group">
-                <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Subject')?></label>
-                <?php echo erLhcoreClassRenderHelper::renderMultiDropdown( array (
-                    'input_name'     => 'subject_id[]',
-                    'optional_field' => erTranslationClassLhTranslation::getInstance()->getTranslation('chat/lists/search_panel','Select subject'),
-                    'selected_id'    => $input->subject_id,
-                    'css_class'      => 'form-control',
-                    'display_name'   => 'name',
-                    'list_function'  => 'erLhAbstractModelSubject::getList',
-                    'list_function_params'  => array('limit' => false)
-                )); ?>
-            </div>
-        </div>
+        <?php include(erLhcoreClassDesign::designtpl('elasticsearch/parts/chat_subject_filter.tpl.php')); ?>
     </div>
 
         <div class="row">
