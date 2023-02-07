d6ee80455da6dbc3577c83434c40a381ca920cbb
Remigijus Kiminas <remdex@gmail.com>
2023-02-07 07:59:02 -0500

    Dark mode fix

--- design/elasticsearchtheme/tpl/lhchat/chat_tabs/information_rows/visitor_title.tpl.php
+++ design/elasticsearchtheme/tpl/lhchat/chat_tabs/information_rows/visitor_title.tpl.php
@@ -1,6 +1,6 @@
 <h6 class="fw-bold">
     <?php if ($chat->email != '') : ?>
-        <a target="_blank" href="<?php echo erLhcoreClassDesign::baseurl('elasticsearch/interactions')?>/(attr)/email/(val)/<?php echo rawurlencode($chat->email) ?>" class="text-dark">
+        <a target="_blank" href="<?php echo erLhcoreClassDesign::baseurl('elasticsearch/interactions')?>/(attr)/email/(val)/<?php echo rawurlencode($chat->email) ?>" class="text-muted">
         <i class="material-icons">open_in_new</i>
     <?php else : ?>
         <i class="material-icons">face</i>
