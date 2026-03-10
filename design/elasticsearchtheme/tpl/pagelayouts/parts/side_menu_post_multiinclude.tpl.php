acf4cc9fdde7f69a9be8d45a4bea17e4aa773906
Remigijus Kiminas <remdex@gmail.com>
2026-03-10 00:45:45 -0400

    Add identifier so others extensions can disable this menu item

--- design/elasticsearchtheme/tpl/pagelayouts/parts/side_menu_post_multiinclude.tpl.php
+++ design/elasticsearchtheme/tpl/pagelayouts/parts/side_menu_post_multiinclude.tpl.php
@@ -1,5 +1,5 @@
 <?php
 if (erLhcoreClassUser::instance()->hasAccessTo('lhelasticsearch', 'use')) {
-    $menuItems[] = array('href' => erLhcoreClassDesign::baseurl('elasticsearch/list'), 'iclass' => 'search', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Search'));
+    $menuItems[] = array('identifier' => 'elastic_chats', 'href' => erLhcoreClassDesign::baseurl('elasticsearch/list'), 'iclass' => 'search', 'text' => erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Search'));
 }
 ?>
